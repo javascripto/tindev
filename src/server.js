951bb4a13507c7c9e9c4cd8e8cb61c2faf6a491a
Yuri Alves <yurialves2@gmail.com>
2019-08-06 20:47:16 -0300

    database connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
+mongoose.connect('mongodb+srv://<user>:<pass>@cluster0-lql7c.mongodb.net/<database>?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 const server = express();
 
