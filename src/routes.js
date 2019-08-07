4cd3d4c91464a382fbeb53232acc406f5366f9fd
Yuri Alves <yurialves2@gmail.com>
2019-08-06 22:11:10 -0300

    created dev,  like and dislike routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
 
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = Router();
 
@@ -7,8 +10,10 @@ routes.get('/', (req, res) => {
   return res.send(`Olá, ${req.query.name || 'Mundo'}`);
 });
 
-routes.post('/devs', (req, res) => {
-  return res.send(req.body);
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
 
 module.exports = routes;
